76fcac18188629a2141af1001b339712347b2ae8
Mike D <mike.d.hoore@telenet.be>
2019-05-16 21:35:49 +0200

    New responsive design

---
+++ V1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>Home Auto</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/jquery-3.4.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row" id="R1">
+        <div class="d-block col-sm-2 col-6 align-self-center" id="R1C1">A</div>
+        <div class="d-none d-sm-block col-sm-4 align-self-center" id="R1C2">
+            <h1>15:00</h1>
+            <h2>15 mei 2019</h2>
+        </div>
+        <div class="d-none d-sm-block col-sm-2 align-self-center text-right" id="R1C3">
+            <h3>IN : 20&deg;C</h3>
+            <h3>OUT : 13&deg;C</h3>
+        </div>
+        <div class="d-none d-sm-block col-sm-2 align-self-center" id="R1C4">
+            <img src="img/weather/sun.svg" width="100px" height="100px">
+        </div>
+        <div class=" col-sm-2 col-6 align-self-center" id="R1C5">E</div>
+    </div>
+    <div class="row" id="R2">
+        <div class="col-sm-2 col-6 align-self-center" id="R2C1">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R2C2">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R2C3">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R2C4">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R2C5">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R2C6">A</div>
+    </div>
+    <div class="row" id="R3">
+        <div class="col-sm-2 col-6 align-self-center" id="R3C1">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R3C2">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R3C3">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R3C4">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R3C5">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R3C6">A</div>
+    </div>
+    <div class="row" id="R4">
+        <div class="col-sm-2 col-6 align-self-center" id="R4C1">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R4C2">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R4C3">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R4C4">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R4C5">A</div>
+        <div class="col-sm-2 col-6 align-self-center" id="R4C6">A</div>
+    </div>
+</div>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="nl">
 <head>
     <title>Home Automation By Mike</title>
+    <link rel="shortcut icon" href="img/logo.ico" type="image/x-icon" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -11,6 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="js/timeanddate.js"></script>
+</head>
 <body>
 
 <div class="container-fluid">
