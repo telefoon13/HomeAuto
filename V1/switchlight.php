f89be947f87344f462d929e21881ff69f4c1d1c9
Mike D <mike.d.hoore@telenet.be>
2019-05-29 17:23:35 +0200

    Intigrated upstair lights + Include all lights in All off

--- V1/switchlight.php
+++ V1/switchlight.php
@@ -1,6 +1,15 @@
 <?php
 include_once ("php/MainHelper.php");
 $return = switchSonOffLight($_GET['ip'], $_GET['port']);
+//Alles uit dan ook staande lamp
+if ($_GET['ip'] == "192.168.178.154" && $_GET['port'] == "2"){
+    $return2 = switchSonOffLight("192.168.178.155", "1");
+    if ($return == "200" && $return2 == "200"){
+        $return = "200";
+    } else {
+        $return = "500";
+    }
+}
 if ($return == "200"){
     $message = "Licht is geschakeld";
 } else {
---
+++ V1/upstairLights.php
@@ -0,0 +1,64 @@
+<div class="row" id="R2">
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R2C1">
+        <a href="index.php?page=switchlight&ip=192.168.178.152&port=1">
+            <img src="img/upStairs/bed.svg" alt="bed" class="w-75">
+            <h5>Julie kamer</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R2C2">
+        <a href="index.php?page=switchlight&ip=192.168.178.152&port=2">
+            <img src="img/upStairs/bedSimple.svg" alt="bed" class="w-75">
+            <h5>Gast kamer</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R2C3">
+        <a href="index.php?page=switchlight&ip=192.168.178.152&port=3">
+            <img src="img/upStairs/stairs.svg" alt="stairs" class="w-75">
+            <h5>Trappen hal</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R2C4">
+        <a href="index.php?page=switchlight&ip=192.168.178.152&port=4">
+            <img src="img/upStairs/shower.svg" alt="shower" class="w-75">
+            <h5>Badkamer</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R2C5">
+        <a href="index.php?page=switchlight&ip=192.168.178.153&port=1">
+            <img src="img/upStairs/mirror.svg" alt="mirror" class="w-75">
+            <h5>Spiegel</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R2C6">
+        <a href="index.php?page=switchlight&ip=192.168.178.153&port=2">
+            <img src="img/upStairs/desk.svg" alt="desk" class="w-75">
+            <h5>Bureau</h5>
+        </a>
+    </div>
+</div>
+<div class="row" id="R3">
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R3C1">
+        <a href="index.php?page=switchlight&ip=192.168.178.153&port=3">
+            <img src="img/upStairs/Masterbedroom.svg" alt="Masterbedroom" class="w-75">
+            <h5>MaPa kamer</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R3C2">
+        <a href="index.php?page=switchlight&ip=192.168.178.153&port=4">
+            <img src="img/upStairs/dressing.svg" alt="dressing" class="w-75">
+            <h5>Dressing</h5>
+        </a>
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R3C3">
+        &emsp;
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R3C4">
+        &emsp;
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R3C5">
+        &emsp;
+    </div>
+    <div class="col-sm-2 col-6 align-self-center text-center" id="R3C6">
+        &emsp;
+    </div>
+</div>
