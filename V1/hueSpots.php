b1002b2b6829a013138a7cab9cd5590093108fb8
Mike D <mike.d.hoore@telenet.be>
2019-05-31 14:51:17 +0200

    Added Spot svg, hue spots page and first 2 functions for HUE

--- V1/hueSpots.php
+++ V1/hueSpots.php
@@ -1,65 +1,62 @@
+<?php
+include_once ("php/hue.php");
+$spot1 = getLightInfo(1);
+$spot2 = getLightInfo(2);
+$spot3 = getLightInfo(3);
+$spot4 = getLightInfo(4);
+$spot5 = getLightInfo(5);
+$spot6 = getLightInfo(6);
+?>
 <div class="row" id="R2">
     <div class="col-sm-2 col-6 align-self-center text-center" id="R2C1">
         <a href="index.php?page=switchlight&ip=192.168.178.150&port=1">
-            <img src="img/downStairs/door.svg" alt="door" class="w-75">
-            <h5>Inkom</h5>
+            <img src="img/downStairs/spotlight.svg" alt="spotlight" class="w-75" style="transform: rotate(90deg) scaleX(-1);">
+            <h5><?= $spot1['name']; ?></h5>
         </a>
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R2C2">
         <a href="index.php?page=switchlight&ip=192.168.178.150&port=2">
-            <img src="img/downStairs/couch.svg" alt="couch" class="w-75">
-            <h5>Zithoek</h5>
+            <img src="img/downStairs/spotlight.svg" alt="spotlight" class="w-75">
+            <h5><?= $spot2['name']; ?></h5>
         </a>
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R2C3">
         <a href="index.php?page=switchlight&ip=192.168.178.155&port=1">
-            <img src="img/downStairs/bedside-table.svg" alt="bedside" class="w-75">
-            <h5>Staande lamp</h5>
+            <img src="img/downStairs/spotlight.svg" alt="spotlight" class="w-75">
+            <h5><?= $spot3['name']; ?></h5>
         </a>
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R2C4">
         <a href="index.php?page=switchlight&ip=192.168.178.150&port=3">
-            <img src="img/downStairs/abc-block.svg" alt="abc" class="w-75">
-            <h5>Speelhoek</h5>
+            <img src="img/downStairs/spotlight.svg" alt="spotlight" class="w-75">
+            <h5><?= $spot4['name']; ?></h5>
         </a>
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R2C5">
         <a href="index.php?page=switchlight&ip=192.168.178.150&port=4">
-            <img src="img/downStairs/kitchen.svg" alt="kitchen" class="w-75">
-            <h5>Keuken</h5>
+            <img src="img/downStairs/spotlight.svg" alt="spotlight" class="w-75">
+            <h5><?= $spot5['name']; ?></h5>
         </a>
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R2C6">
         <a href="index.php?page=switchlight&ip=192.168.178.151&port=1">
-            <img src="img/downStairs/restaurant.svg" alt="restaurant" class="w-75">
-            <h5>Eetkamer</h5>
+            <img src="img/downStairs/spotlight.svg" alt="spotlight" class="w-75">
+            <h5><?= $spot6['name']; ?></h5>
         </a>
     </div>
 </div>
 <div class="row" id="R3">
     <div class="col-sm-2 col-6 align-self-center text-center" id="R3C1">
-        <a href="index.php?page=switchlight&ip=192.168.178.151&port=2">
-            <img src="img/downStairs/toilet-paper.svg" alt="toilet-paper" class="w-75">
-            <h5>Toilet</h5>
-        </a>
+        &emsp;
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R3C2">
-        <a href="index.php?page=switchlight&ip=192.168.178.151&port=3">
-            <img src="img/downStairs/terrace.svg" alt="terrace" class="w-75">
-            <h5>Terras</h5>
-        </a>
+        &emsp;
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R3C3">
-        <a href="index.php?page=switchlight&ip=192.168.178.151&port=4">
-            <img src="img/downStairs/shed.svg" alt="shed" class="w-75">
-            <h5>Tuinhuis</h5>
-        </a>
+        &emsp;
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R3C4">
-        <a href="index.php?page=hueSpots">
-            <img src="img/downStairs/grass.svg" alt="grass" class="w-75">
-            <h5>Tuinspots</h5>
-        </a>
+        &emsp;
     </div>
     <div class="col-sm-2 col-6 align-self-center text-center" id="R3C5">
         &emsp;
--- V1/php/hue.php
+++ V1/php/hue.php
@@ -1,2 +1,70 @@
 <?php
+include_once ("MainHelper.php");
+
 $user = "hI5h45SfpDgZRc4PIzsoYeGJus5a5iCiPj6aeawm";
+$baseUrl = "http://192.168.178.157/api/".$user;
+$getLight = $baseUrl."/lights/";
+
+//Function to get info from a light
+function getLightInfo($id){
+    global $getLight;
+    $result = cUrl($getLight.$id);
+    if ($result[0] == "200"){
+        $values = $result[1];
+        $lightInfo = array(
+            "on" => $values['state']['on'],
+            "bri" => $values['state']['bri'],
+            "hue" => $values['state']['hue'],
+            "sat" => $values['state']['sat'],
+            "effect" => $values['state']['effect'],
+            "x" => $values['state']['xy'][0],
+            "y" => $values['state']['xy'][1],
+            "swupdate_state" => $values['swupdate']['state'],
+            "name" => $values['name']
+        );
+        return $lightInfo;
+    } else {
+        return $result[0];
+    }
+}
+
+
+
+
+//Function to convert the x,y and bri to a HEX color
+//Source : https://stackoverflow.com/questions/22894498/philips-hue-convert-xy-from-api-to-hex-or-rgb
+function xyBriToRgb($x,$y,$bri)
+{
+    $z = 1.0 - $x - $y;
+    $Y = $bri / 255.0;
+    $X = ($Y / $y) * $x;
+    $Z = ($Y / $y) * $z;
+
+    $r = $X * 1.612 - $Y * 0.203 - $Z * 0.302;
+    $g = ($X * -1) * 0.509 + $Y * 1.412 + $Z * 0.066;
+    $b = $X * 0.026 - $Y * 0.072 + $Z * 0.962;
+
+    $r = $r <= 0.0031308 ? 12.92 * $r : (1.0 + 0.055) * pow($r, (1.0 / 2.4)) - 0.055;
+    $g = $g <= 0.0031308 ? 12.92 * $g : (1.0 + 0.055) * pow($g, (1.0 / 2.4)) - 0.055;
+    $b = $b <= 0.0031308 ? 12.92 * $b : (1.0 + 0.055) * pow($b, (1.0 / 2.4)) - 0.055;
+
+    $maxValue = max( $r , $g, $b );
+
+    $r = $r / $maxValue;
+    $g = $g / $maxValue;
+    $b = $b / $maxValue;
+
+    $r = $r * 255; if ($r < 0) $r = 255;
+    $g = $g * 255; if ($g < 0) $g = 255;
+    $b = $b * 255; if ($b < 0) $b = 255;
+
+    $r = dechex(round($r));
+    $g = dechex(round($g));
+    $b = dechex(round($b));
+
+    if (strlen($r) < 2)     $r = "0" . $r;
+    if (strlen($g) < 2)     $g = "0" . $g;
+    if (strlen($b) < 2)     $b = "0" . $b;
+
+    return "#".$r.$g.$b;
+}
