15d53e4a49087ea5deb23b39bc46c8dcbd38aa3e
Mike D <mike.d.hoore@telenet.be>
2019-06-07 20:22:11 +0200

    Hue + staande lamp initgration with all on and off

--- switchlight.php
+++ switchlight.php
@@ -1,14 +1,26 @@
 <?php
 include_once("php/MainHelper.php");
+include_once("php/hue.php");
 $return = switchSonOffLight($_GET['ip'], $_GET['port'], "");
-//Alles uit dan ook staande lamp
+//Alles uit dan ook staande lamp & Hue spots
 if ($_GET['ip'] == "192.168.178.154" && $_GET['port'] == "2"){
     $return2 = switchSonOffLight("192.168.178.155", "1", "off");
-    if ($return == "200" && $return2 == "200"){
+    $return3 = updateGroup(1,false);
+    if ($return == "200" && $return2 == "200" && $return3 == "200"){
         $return = "200";
     } else {
         $return = "500";
     }
+}
+//Alles aan dan ook staande lamp & Hue spots
+if ($_GET['ip'] == "192.168.178.154" && $_GET['port'] == "1"){
+	$return2 = switchSonOffLight("192.168.178.155", "1", "on");
+	$return3 = updateGroup(1,true);
+	if ($return == "200" && $return2 == "200" && $return3 == "200"){
+		$return = "200";
+	} else {
+		$return = "500";
+	}
 }
 if ($return == "200"){
     $message = "Licht is geschakeld";
