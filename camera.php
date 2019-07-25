6c1c1ab9da165d9d8f1397e7d7ec298f8e4948dc
Mike D <mike.d.hoore@telenet.be>
2019-07-25 19:04:58 +0200

    Added doorbell camera

--- camera.php
+++ camera.php
@@ -2,18 +2,34 @@
 if ($_GET['camid'] == null || empty($_GET['camid'])){
 ?>
     <div class="row" id="R2">
-        <div class="col-sm-6 col-12 align-self-center text-center" id="R2C1" style="padding-left: 5px;">
+        <div class="col-sm-6 col-12 align-self-center text-center" id="R2C1">
             <a href="camera.php?camid=1">
-                <img width="500px" height="283px" src="
+                <img width="320px" height="180px" src="
             http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=1&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
             " ></a>
         </div>
-        <div class="col-sm-6 col-12 align-self-center text-center" id="R2C1" style="padding-left: 5px;">
+        <div class="col-sm-6 col-12 align-self-center text-center" id="R2C2">
             <a href="camera.php?camid=2">
-                <img width="500px" height="283px" src="
-            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&maxfps=30&buffer=1000&user=viewer2&pass=viewer2
+                <img width="320px" height="180px" src="
+            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
             " >
             </a>
+        </div>
+    </div>
+
+    <div class="row" id="R2">
+        <div class="col-sm-6 col-12 align-self-center text-center" id="R3C1" >
+            <a href="camera.php?camid=4">
+                <img width="320px" height="180px" src="
+            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=4&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
+            " ></a>
+        </div>
+        <div class="col-sm-6 col-12 align-self-center text-center" id="R3C2" style="padding-left: 5px;">
+           <!-- <a href="camera.php?camid=2">
+                <img width="320px" height="180px" src="
+            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=5&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
+            " >
+            </a>-->
         </div>
     </div>
 <?php
