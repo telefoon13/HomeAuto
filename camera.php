a1d32dc97cf9bf38ba2d92aa8feea07d5ad19b8f
Mike D <mike.d.hoore@telenet.be>
2019-11-09 09:53:41 +0100

    Camera changes

--- camera.php
+++ camera.php
@@ -3,15 +3,15 @@ if ($_GET['camid'] == null || empty($_GET['camid'])){
 ?>
     <div class="row" id="R2">
         <div class="col-sm-6 col-12 align-self-center text-center" id="R2C1">
-            <a href="index.php?page=camera&camid=1">
-                <img width="320px" height="180px" src="
-            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=1&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
-            " ></a>
-        </div>
-        <div class="col-sm-6 col-12 align-self-center text-center" id="R2C2">
             <a href="index.php?page=camera&camid=2">
                 <img width="320px" height="180px" src="
             http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
+            " ></a>
+        </div>
+        <div class="col-sm-6 col-12 align-self-center text-center" id="R2C2">
+            <a href="index.php?page=camera&camid=5">
+                <img width="320px" height="180px" src="
+            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=5&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
             " >
             </a>
         </div>
@@ -25,9 +25,9 @@ if ($_GET['camid'] == null || empty($_GET['camid'])){
             " ></a>
         </div>
         <div class="col-sm-6 col-12 align-self-center text-center" id="R3C2" style="padding-left: 5px;">
-           <a href="index.php?page=camera&camid=5">
+           <a href="index.php?page=camera&camid=1">
                 <img width="320px" height="180px" src="
-            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=5&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
+            http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=1&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer
             " >
             </a>
         </div>
---
+++ cameraFull.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<title>Home Auto</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/main.css">
+	<script src="js/jquery-3.4.1.js"></script>
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+<div class="row" id="R2">
+	<div class="col-6 align-self-center text-center" id="R2C1">
+		<img width="100%" src="http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=2&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer" >
+	</div>
+	<div class="col-6 align-self-center text-center" id="R2C2">
+		<img width="100%" src="http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=5&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer" >
+	</div>
+</div>
+<div class="row" id="R2">
+	<div class="col-6 align-self-center text-center" id="R3C1">
+		<img width="100%" src="http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=4&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer">
+	</div>
+	<div class="col-6 align-self-center text-center" id="R3C2">
+		<img width="100%" src="http://192.168.178.204/cgi-bin/nph-zms?mode=jpeg&monitor=1&scale=100&maxfps=30&buffer=1000&user=viewer&pass=viewer">
+	</div>
+</div>
+</div>
+</body>
