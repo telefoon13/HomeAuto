5b530daa3a156b926be6bab7d9b07ad37b28d1a2
Mike D <mike.d.hoore@telenet.be>
2020-01-12 10:22:34 +0100

    fix background bug

--- js/timeanddate.js
+++ js/timeanddate.js
@@ -10,8 +10,9 @@ function date_time(id)
     days = new Array('zon', 'maa', 'din', 'woe', 'don', 'vri', 'zat');
     h = date.getHours();
     if (h >= 22 || h <= 6){
-
         $("body").css("background-color", "black");
+    } else {
+        $("body").css("background-color", "azure");
     }
     if(h<10)
     {
@@ -24,6 +25,6 @@ function date_time(id)
     }
     result = '<h1 class="display-3" style="font-weight: bold;">' + h + ':' + m + '</h1><h3>' + days[day] + ' ' + d + ' ' + months[month] + ' ' + year + '</h3>';
     document.getElementById(id).innerHTML = result;
-    setTimeout('date_time("'+id+'");','10000');
+    setTimeout('date_time("'+id+'");','60000');
     return true;
 }
